884952be60a938c52473729bdb5819e401acd8ba
sreejarajan <sreeja080895@gmail.com>
2016-07-18 19:52:27 +0530

    Create posneg.c

---
+++ posneg.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int a;
+clrscr();
+scanf("%d",&a);
+if(a>0)
+printf("\nthe value is positive");
+else if(a<0)
+printf("\nthe value is negative");
+else
+printf("\nthe value is zero");
+getch();
+}
+
